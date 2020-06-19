4d76f3687d201516974b897c20026c0ce7c254db
humber <husani925@gmail.com>
2020-06-19 13:01:51 -0500

    Updates

---
+++ precache-manifest.87ffd6297c41a79a6111912cf1859b30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2908caba95976367dd019e2b3b2f11d2",
+    "url": "/husani925/react.git/index.html"
+  },
+  {
+    "revision": "3f01ceadd75eaf42f386",
+    "url": "/husani925/react.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56f8ee869811aaa3f131",
+    "url": "/husani925/react.git/static/js/2.d97a50cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/husani925/react.git/static/js/2.d97a50cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f01ceadd75eaf42f386",
+    "url": "/husani925/react.git/static/js/main.71e82d27.chunk.js"
+  },
+  {
+    "revision": "f0e43b9045aa6ca69554",
+    "url": "/husani925/react.git/static/js/runtime-main.71c276fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/husani925/react.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
